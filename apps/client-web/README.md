2686cb8b0669dea526552d274d4e1c2be82b643c
Athul Anoop <athulanoop2020@gmail.com>
2025-07-17 19:44:13 +0530

    add: NextJS project setup

---
+++ apps/client-web/README.md
@@ -0,0 +1,3 @@
+# client-web
+
+`client-web` is the web client for the Enterprise Resource Planning (ERP) solution for Rehabilitation Plantation Limited, Punalur. The web client provides access to the backend APIs facilitating database operations, data visualization, banking, etc... associated with the ERP.
---
+++ apps/client-web/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Rehabilitation Plantation Limited - ERP",
+    description:
+        "Enterprise Resource Planning solution for Rehabilitation Plantation Limited, Punalur",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
