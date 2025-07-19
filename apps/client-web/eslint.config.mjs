cd46cbae80f1a90f8c87d6b915eae97c58995316
Athul Anoop <athulanoop2020@gmail.com>
2025-07-19 06:04:49 +0530

    update: formatting (4 space tabs)

--- apps/client-web/eslint.config.mjs
+++ apps/client-web/eslint.config.mjs
@@ -1,18 +1,18 @@
 import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
+    baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.config({
-    extends: ["next", "next/core-web-vitals", "next/typescript"],
-    settings: {
-      next: {
-        rootDir: "apps/client-web",
-      },
-    },
-  }),
+    ...compat.config({
+        extends: ["next", "next/core-web-vitals", "next/typescript"],
+        settings: {
+            next: {
+                rootDir: "apps/client-web",
+            },
+        },
+    }),
 ];
 
 export default eslintConfig;
