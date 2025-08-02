676b44b9ceb54450aea4c6fd5475b326c5c75fa1
Athul Anoop <athulanoop2020@gmail.com>
2025-08-02 08:49:21 +0530

    update: import alias

--- apps/client-web/src/app/page.tsx
+++ apps/client-web/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@client-web/components/ui/button";
+
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <Button>This is a button</Button>
         </div>
     );
 }
