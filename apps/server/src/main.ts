8f3ef8935c1fcfafb8d8877be613d2f6de7915d0
Athul Anoop <athulanoop2020@gmail.com>
2025-08-05 06:42:16 +0530

    update: environment variable usage

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+const PORT = process.env.PORT ?? 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT);
 }
 bootstrap()
-  .then(() => console.log(`Server is running on ${process.env.PORT}`))
-  .catch(console.error);
+    .then(() => console.log(`Server is running on ${PORT}`))
+    .catch(console.error);
